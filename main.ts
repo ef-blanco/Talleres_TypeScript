5a167264ad84c0a7b99e681232660f0c9d587d46
Emmanuel Blanco <ef.blanco@uniandes.edu.co>
2025-04-04 20:02:15 -0500

    mostrarCard añadido a main. Función pendiente de revisión

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { dataTvShows } from './dataTvShows.js';
 
 let tvShowsTbody: HTMLElement = document.getElementById("tvShows")!;
 const avrgSeasonsElem: HTMLElement = document.getElementById("avrgSeasons")!;
+let card: HTMLElement = document.getElementById("col-card")!;
 
 function renderTvShowsOnTable(tvShows: TvShow[]):void
 {
@@ -10,7 +11,7 @@ function renderTvShowsOnTable(tvShows: TvShow[]):void
     tvShows.forEach((tvShow)=>{
         let trElement = document.createElement("tr");
         trElement.innerHTML =  `<th scope="row">${tvShow.pos}</th>
-                                <button type="button" class="btn btn-link">${tvShow.name}</button>
+                                <button type="button" onclick="mostrarCard(${tvShow.pos})" class="btn btn-link">${tvShow.name}</button>
                                 <td>${tvShow.channel}</td>
                                 <td>${tvShow.seasons}</td>`;
     tvShowsTbody.appendChild(trElement);
@@ -30,3 +31,29 @@ function getAverageSeasons(tvShows: TvShow[]): number
 }
 
 avrgSeasonsElem.innerHTML = `${getAverageSeasons(dataTvShows)}`
+
+function mostrarCard(numSerie: number):void
+{
+    if (numSerie == 1)
+    {
+        card.innerHTML = `<div class="card">
+                            <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqhl-FYhFVsfyBUHTvx1e4oxr6t4AbMocmC56U-L6M05l8iqnDKL4YBvzWfy_441PXgOc&usqp=CAU" alt="ejemplo" width="100" height="180">
+                             <div class="card-body">
+                               <h3 class="card-title">${dataTvShows[0].name}</h3>
+                               <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nostrum est iure ex nihil reiciendis doloribus cupiditate aut deleniti ducimus. Facilis ullam deleniti cupiditate debitis, sed nemo nihil consequuntur aliquid!</p>
+                               <a href="" class="card-url">url</a>
+                           </div>
+                         </div>`
+    }
+    else if (numSerie == 2)
+        {
+            card.innerHTML = `<div class="card">
+                                <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqhl-FYhFVsfyBUHTvx1e4oxr6t4AbMocmC56U-L6M05l8iqnDKL4YBvzWfy_441PXgOc&usqp=CAU" alt="ejemplo" width="100" height="180">
+                                 <div class="card-body">
+                                   <h3 class="card-title">${dataTvShows[1].name}</h3>
+                                   <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nostrum est iure ex nihil reiciendis doloribus cupiditate aut deleniti ducimus. Facilis ullam deleniti cupiditate debitis, sed nemo nihil consequuntur aliquid!</p>
+                                   <a href="" class="card-url">url</a>
+                               </div>
+                             </div>`
+        }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,12 @@
 import { dataTvShows } from './dataTvShows.js';
 var tvShowsTbody = document.getElementById("tvShows");
 var avrgSeasonsElem = document.getElementById("avrgSeasons");
+var card = document.getElementById("col-card");
 function renderTvShowsOnTable(tvShows) {
     console.log("Desplegando TV Shows");
     tvShows.forEach(function (tvShow) {
         var trElement = document.createElement("tr");
-        trElement.innerHTML = "<th scope=\"row\">".concat(tvShow.pos, "</th>\n                                <button type=\"button\" class=\"btn btn-link\">").concat(tvShow.name, "</button>\n                                <td>").concat(tvShow.channel, "</td>\n                                <td>").concat(tvShow.seasons, "</td>");
+        trElement.innerHTML = "<th scope=\"row\">".concat(tvShow.pos, "</th>\n                                <button type=\"button\" onclick=\"mostrarCard(").concat(tvShow.pos, ")\" class=\"btn btn-link\">").concat(tvShow.name, "</button>\n                                <td>").concat(tvShow.channel, "</td>\n                                <td>").concat(tvShow.seasons, "</td>");
         tvShowsTbody.appendChild(trElement);
     });
 }
@@ -18,3 +19,11 @@ function getAverageSeasons(tvShows) {
     return resp;
 }
 avrgSeasonsElem.innerHTML = "".concat(getAverageSeasons(dataTvShows));
+function mostrarCard(numSerie) {
+    if (numSerie == 1) {
+        card.innerHTML = "<div class=\"card\">\n                            <img class=\"card-img-top\" src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqhl-FYhFVsfyBUHTvx1e4oxr6t4AbMocmC56U-L6M05l8iqnDKL4YBvzWfy_441PXgOc&usqp=CAU\" alt=\"ejemplo\" width=\"100\" height=\"180\">\n                             <div class=\"card-body\">\n                               <h3 class=\"card-title\">".concat(dataTvShows[0].name, "</h3>\n                               <p class=\"card-text\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nostrum est iure ex nihil reiciendis doloribus cupiditate aut deleniti ducimus. Facilis ullam deleniti cupiditate debitis, sed nemo nihil consequuntur aliquid!</p>\n                               <a href=\"\" class=\"card-url\">url</a>\n                           </div>\n                         </div>");
+    }
+    else if (numSerie == 2) {
+        card.innerHTML = "<div class=\"card\">\n                                <img class=\"card-img-top\" src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqhl-FYhFVsfyBUHTvx1e4oxr6t4AbMocmC56U-L6M05l8iqnDKL4YBvzWfy_441PXgOc&usqp=CAU\" alt=\"ejemplo\" width=\"100\" height=\"180\">\n                                 <div class=\"card-body\">\n                                   <h3 class=\"card-title\">".concat(dataTvShows[1].name, "</h3>\n                                   <p class=\"card-text\">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et nostrum est iure ex nihil reiciendis doloribus cupiditate aut deleniti ducimus. Facilis ullam deleniti cupiditate debitis, sed nemo nihil consequuntur aliquid!</p>\n                                   <a href=\"\" class=\"card-url\">url</a>\n                               </div>\n                             </div>");
+    }
+}
